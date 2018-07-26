c07fed6f1ead298d368e17e609ee05a112d3f8f5
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2018-07-26 10:18:39 -0300

    aula_02_Concluida

--- Alura/javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ Alura/javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -5,31 +5,24 @@ class Negociacao {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
-
+        Object.freeze(this);
     }
-
-    obtemVolume {
-
-        return this._quantidade * this._valor;
-    }
-
-
 
     get volume() {
 
       return this._quantidade * this._valor;
 
-  }
+    }
 
-  get data() {
-      return this._data;
-  }
+    get data() {
+        return this._data;
+    }
 
-  get quantidade() {
-      return this._quantidade;
-  }
+    get quantidade() {
+        return this._quantidade;
+    }
 
-  get valor() {
-      return this._valor;
+    get valor() {
+        return this._valor;
+    }
   }
-}
